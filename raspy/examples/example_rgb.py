f3013365f9a8b68119d2edcce3d09088ca277c00
thunderboltsid <sk191968>
2016-05-12 16:12:45 +0200

    Add example with rgb

---
+++ raspy/examples/example_rgb.py
@@ -0,0 +1,35 @@
+# Import library
+from raspy.rasda import *
+
+# Initialize Connection and database instance
+con = Connection()
+db = con.database("RASBASE")
+
+# List Collections on database
+print(db.collections)
+
+# Initialize collection of your choice
+col = RasCollection("rgb")
+
+# Perform operations
+col /= 3
+col -= 10
+
+# Specify database to use
+col.use_db(db)
+
+# Verify query
+str(col.query)
+
+# Evaluate query
+data = col.eval()
+
+# Access the array in numpy format
+arr = data.to_array()
+
+# Convert to image
+data.to_image("example_rgb.png")
+
+# Use condensers
+avg = col.avg_cells().eval()
+
