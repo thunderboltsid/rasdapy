7b1eddd28584b6e20966042429a2521aa9f45196
Siddharth Shukla <siddharthshukla@outlook.com>
2016-08-23 05:06:46 +0200

    rename lib to rasdapy

--- setup.py
+++ setup.py
@@ -83,13 +83,13 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['examples']),
+    packages=find_packages(exclude=['docs', 'examples', 'scripts', 'tests']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['grpcio', 'protobuf', 'numpy'],
+    install_requires=['grpcio>=1.0.0', 'protobuf>=3.0.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
