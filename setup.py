afdf030261e453a0e625ae28c4e3a2efafabcb75
Siddharth Shukla <siddharthshukla@outlook.com>
2016-08-23 23:18:46 +0200

    change topic to libraries

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Database',
+        'Topic :: Software Development :: Libraries',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: LGPL License',
