7ca2b9cf4f1c28f8b579120fca9c12390af878d2
siddharthshukla <siddharthshukla@outlook.com>
2016-08-26 15:22:51 +0200

    bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.4',
+    version='0.0.6',
 
     description='Python interface to rasdaman',
     long_description=long_description,
